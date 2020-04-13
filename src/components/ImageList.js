c23872662d98123f09603f46fe914177966cc39b
ycbencheng <bcyc2004@gmail.com>
2020-04-13 07:28:19 -0600

    Refactor to image card comp

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,13 +1,11 @@
 import React from "react";
 
 import "./ImageList.css";
-
+import ImageCard from "./ImageCard";
 const ImageList = (props) => {
   console.log("props.images", props.images);
   const images = props.images.map((image) => {
-    return (
-      <img src={image.urls.small} key={image.id} alt={image.description} />
-    );
+    return <ImageCard key={image.id} image={image} />;
   });
 
   return <div className="image-list">{images}</div>;
